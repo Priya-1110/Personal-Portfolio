c41431c64361cb90bce5e7236d1657e2c32a1f0d
Priya-1110 <priyashan112002@gmail.com>
2024-11-24 00:32:15 +0000

    vercel added 3

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -232,7 +232,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="alt image"></img>
     </section>
   )
 }
